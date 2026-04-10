5f3ae6d1ae5bbe4fb025c84346c28d2c9f6bc544
Gaurav Rajendra Deshmane <114251900+gaurav7447@users.noreply.github.com>
2026-04-10 12:04:13 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,14 @@
-print("Hello from AWS CI Pipeline 🚀")
+import unittest
+
+# Function with intentional bug
+def add(a, b):
+    return a - b   # ❌ BUG (should be +)
+
+# Test cases
+class TestCalculator(unittest.TestCase):
+
+    def test_add(self):
+        self.assertEqual(add(2, 3), 5)
+
+if __name__ == "__main__":
+    unittest.main()
